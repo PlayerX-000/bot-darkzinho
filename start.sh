582144073af66ff4d2c5c36d68e83737d971a4f9
Kauan Henrique <kauan.mghenrique@gmail.com>
2022-01-26 22:29:29 -0300

    update

--- start.sh
+++ start.sh
@@ -1,6 +1,14 @@
-apt update && apt upgrade
-pkg install git
+apt update 
+apt upgrade
 pkg install python
+pkg install git
 apt install nodejs
-npm install
+npm install yarn
+yarn
+yarn add @adiwajshing/baileys
+yarn add axios
+yarn add fluent-ffmpeg
+yarn add qrcode-terminal
+yarn add streamifier
+yarn add wa-sticker-formatter
 git submodule add git://github.com/schaermu/node-fluent-ffmpeg.git vendor/fluent-ffmpeg
